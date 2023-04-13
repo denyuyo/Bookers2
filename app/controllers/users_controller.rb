6bc28ec41cc46584309c3a2a17e1c01248daed7a
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-04-13 11:32:22 +0000

    Ok

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_registration_path
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+end
