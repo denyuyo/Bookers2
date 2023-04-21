9e8ba8535a7892da4ce019d076c24ea6dc43a5b0
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-04-21 16:03:12 +0000

    Reduced Errors To 4

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :user
-  
-  validates :body,
-       length: { minimum: 0, maximum: 200 }
-  
+
+  validates :title, uniqueness: true,length: { minimum: 2, maximum: 20 }
+  validates :body, length: { maximum: 200 }
+
   validates :title, presence: true
   validates :body, presence: true
+
 end
