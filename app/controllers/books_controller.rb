184d8c48030082069a3268261ba7d92137295ea6
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-04-22 10:13:21 +0000

    Ok

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -63,7 +63,7 @@ class BooksController < ApplicationController
   def ensure_correct_user
     @book =Book.find(params[:id])
      unless @book.user == current_user
-     redirect_to user_path(user.id)
+     redirect_to books_path
      end
   end
 end
